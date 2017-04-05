26be04048864233a4b7b5645e8e583c578acebb8
Rafael Borges <razborges@me.com>
2017-04-05 18:17:32 -0300

    Atualizando para receber grafos em .txt

--- Trabalho2/bfs.py
+++ Trabalho2/bfs.py
@@ -6,13 +6,15 @@ __author__ = 'Rafael Borges'
 Ex. de implementação do algoritmo BFS (Busca em largura em Grafo) imprimindo todos os vértices do grafo
 '''
 
-def imprime_vertices_bfs(grafo, inicio):
+import sys, os
+
+def bfs(grafo, inicio):
     q = []
     visitado = []
     pais = {}
     q.append(inicio)
     visitado.append(inicio)
-    print(inicio)
+    print(inicio, end='\t')
     while len(q) != 0:
         u = q.pop(0)
         for vertice in grafo[u]:
@@ -20,15 +22,33 @@ def imprime_vertices_bfs(grafo, inicio):
                 pais.update({vertice: u})
                 visitado.append(vertice)
                 q.append(vertice)
-                print(vertice)
+                print(vertice, end='\t')
+    print('\n')
 
 if __name__ == '__main__':
-    grafo = {
-        '1': ['2', '3', '4'],
-        '2': ['1', '3', '4'],
-        '3': ['1', '2'],
-        '4': ['2', '5'],
-        '5': ['4']
-    }
 
-    imprime_vertices_bfs(grafo, '1')
+    if len(sys.argv) != 3:
+        sys.stderr.write('Erro na chamada do comando.\n')
+        sys.stderr.write('Para executar digite: \'python bfs.py NOME_ARQUIVO.TXT VETOR_INICIO >>> onde NOME_ARQUIVO.TXT é o nome do arquivo que contém o grafo, e, VETOR_INICIO é o valor do vetor por onde deverá começar a ser feita a leitura.\n')
+        sys.exit()
+
+    nome_arquivo = sys.argv[1]
+    vetor_inicio = sys.argv[2]
+
+    try:
+        arquivo = open(nome_arquivo, 'r')
+    except IOError:
+        sys.stderr.write('Erro ao tentar ler o arquivo, verifique se está válido.')
+        sys.exit()
+
+    grafo = {}
+
+    for line in arquivo:
+        u, v = map(int, line.split())
+        if u not in grafo:
+            grafo[u] = [v]
+        else:
+            grafo[u].append(v)
+
+    print('*** Vertices usando BFS ***')
+    bfs(grafo, int(vetor_inicio))
--- Trabalho2/dfs.py
+++ Trabalho2/dfs.py
@@ -6,27 +6,29 @@ __author__ = 'Rafael Borges'
 Ex. de implementação do algoritmo DFS (Busca em profundidade em Grafo) imprimindo todos os vértices do grafo
 '''
 
-def imprimindo_vertices_dfs_recursivo(grafo, visitado):
+import sys, os
+
+def dfs_recursivo(grafo, visitado):
     for vertice in grafo:
         if vertice not in visitado:
             print(vertice, end='\t')
-            visita_dfs(grafo, vertice, visitado)
-    print('')
+            dfs_recursivo_visita(grafo, vertice, visitado)
+    print('\n')
 
-def visita_dfs(grafo, vertice, visitado):
+def dfs_recursivo_visita(grafo, vertice, visitado):
     visitado.append(vertice)
     for vertice_aux in grafo[vertice]:
         if vertice_aux not in visitado:
             print(vertice_aux, end='\t')
-            visita_dfs(grafo, vertice_aux, visitado)
+            dfs_recursivo_visita(grafo, vertice_aux, visitado)
 
-def imprimindo_vertices_dfs_iterativo(grafo, visitado, pilha):
+def dfs_iterativo(grafo, visitado, pilha):
     for vertice in grafo:
         if vertice not in visitado:
-            iterativo_dfs(grafo, vertice, visitado, pilha)
-    print('')
+            dfs_iterativo_visita(grafo, vertice, visitado, pilha)
+    print('\n')
 
-def iterativo_dfs(grafo, vertice, visitado, pilha):
+def dfs_iterativo_visita(grafo, vertice, visitado, pilha):
     pilha.append(vertice)
     while len(pilha) != 0:
         vertice = pilha.pop(0)
@@ -36,22 +38,40 @@ def iterativo_dfs(grafo, vertice, visitado, pilha):
             for vertice_aux in grafo[vertice]:
                 pilha.append(vertice_aux)
 
-if __name__ == '__main__':
-    grafo = {
-        '1': ['2', '4', '5'],
-        '2': ['1', '4', '6'],
-        '3': ['7'],
-        '4': ['1', '2', '5'],
-        '5': ['1', '4'],
-        '6': ['2'],
-        '7': ['3']
-    }
-
+def dfs_recursao(grafo):
     visitado = []
-    visitado2 = []
-    pilha = []
+    dfs_recursivo(grafo, visitado)
 
-    print('Vertices usando DFS Recursivo:')
-    imprimindo_vertices_dfs_recursivo(grafo, visitado)
-    print('Vertices usando DFS Iterativo:')
-    imprimindo_vertices_dfs_iterativo(grafo, visitado2, pilha)
+def dfs(grafo):
+    visitado = []
+    pilha = []
+    dfs_iterativo(grafo, visitado, pilha)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        sys.stderr.write('Erro na chamada do comando.\n')
+        sys.stderr.write('Para executar digite: \'python dfs.py NOME_ARQUIVO.TXT >>> onde NOME_ARQUIVO.TXT é o nome do arquivo que contém o grafo.\n')
+        sys.exit()
+
+    nome_arquivo = sys.argv[1]
+
+    try:
+        arquivo = open(nome_arquivo, 'r')
+    except IOError:
+        sys.stderr.write('Erro ao tentar ler o arquivo, verifique se está válido.')
+        sys.exit()
+
+    grafo = {}
+
+    for line in arquivo:
+        u, v = map(int, line.split())
+        if u not in grafo:
+            grafo[u] = [v]
+        else:
+            grafo[u].append(v)
+
+    print('*** Vertices usando DFS Recursivo ***')
+    dfs_recursao(grafo)
+    print('*** Vertices usando DFS Iterativo ***')
+    dfs(grafo)
